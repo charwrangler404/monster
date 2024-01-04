052411b1f0902fd5ce6aa819fdf07b81f09707c5
Elia Farin <efarin@inoc.com>
2024-01-04 13:59:25 -0600

    Added some basic class logic

--- src/classes.py
+++ src/classes.py
@@ -10,11 +10,13 @@ class Character:
         self.data = json.loads(template)
 
     def heal(self, heal):
+        """Called to heal this object"""
         self.hp += min(self.data['max_hp'] - self.hp, heal)
 
     def damage(self, damage):
+        """Called to deal damage to this object"""
         self.hp -= damage
-        if self.hp < 0:
+        if self.hp < 1:
             self.alive = False
             
 
@@ -53,6 +55,7 @@ class Player(Character):
         if failure <= 3:
             self.alive = False
             self.hp = -1
+            self.last_stand_status = False
         elif success <= 3:
             self.last_stand_status = False
             self.hp = 1
@@ -61,3 +64,20 @@ class Player(Character):
 
 class InitiativeOrder:
     order = []
+    position = 100
+    next_init= []
+
+    def advance_order(self):
+        """Called to advance the order to the next initiative group"""
+        self.position -= 1
+        for character in self.order:
+            if character.init == self.position:
+                self.next_init.append(character)
+        self.next_init = sorted(self.order, key=lambda character: character.data['dex'])
+        
+        if len(self.next_init) == 0:
+            self.advance_order()
+
+    def add_to_order(self, character):
+        """Called to add a character object to the order"""
+        self.order.append(character)
